0b6af4beb1ff7742b63cee5dcbcf392a10995b15
ansaal <jhulsch.jh@gmail.com>
2023-07-30 15:21:59 -0700

    init

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,10 +1,12 @@
 <template>
-  Annekatrin Shaw
-  Counsellor, MC RCC
-  Tel: 604-593-3522
-  Fax: 604-593-4480
-  Suite 201, 11957 80th Ave.
-  Delta BC V6C 0E1
+  <div>
+    Annekatrin Shaw
+    Counsellor, MC RCC
+    Tel: 604-593-3522
+    Fax: 604-593-4480
+    Suite 201, 11957 80th Ave.
+    Delta BC V6C 0E1
+  </div>
 </template>
 
 

7149e3346f2668137c8e48ec88a76e042525ac12
ansaal <jhulsch.jh@gmail.com>
2023-07-30 16:09:27 -0700

    router #

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,26 @@
   <v-app id="inspire">
     <v-navigation-drawer v-if="mdAndDown" class="bg-background" v-model="drawer">
       <v-list color="background">
-        <v-list-item href="#/aboutme" prepend-icon="mdi-account" title="About me">
+        <v-list-item to="aboutme" prepend-icon="mdi-account" title="About me">
         </v-list-item>
         <v-divider></v-divider>
-        <v-list-item href="#/services" prepend-icon="mdi-book-open-variant" title="Services">
+        <v-list-item to="services" prepend-icon="mdi-book-open-variant" title="Services">
         </v-list-item>
         <v-divider/>
-        <v-list-item href="#/contact" prepend-icon="mdi-phone" title="Contact">
+        <v-list-item to="contact" prepend-icon="mdi-phone" title="Contact">
         </v-list-item>
       </v-list>
 
     </v-navigation-drawer>
     <v-app-bar color="background">
       <v-app-bar-nav-icon v-if="mdAndDown" @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title><a href="/" style="color: black;text-decoration: none"> Counselling Solutions for Women </a>
+      <v-toolbar-title>
+        <router-link to="/" style="color: black;text-decoration: none"> Counselling Solutions for Women</router-link>
       </v-toolbar-title>
 
-      <v-btn href="#/aboutme" variant="tonal" style="margin-right: 12px" v-if="lgAndUp"> About me</v-btn>
-      <v-btn href="#/services" variant="tonal" style="margin-right: 12px" v-if="lgAndUp"> Services</v-btn>
-      <v-btn href="#/contact" variant="tonal" v-if="lgAndUp"> Contact</v-btn>
+      <v-btn to="aboutme" variant="tonal" style="margin-right: 12px" v-if="lgAndUp"> About me</v-btn>
+      <v-btn to="services" variant="tonal" style="margin-right: 12px" v-if="lgAndUp"> Services</v-btn>
+      <v-btn to="contact" variant="tonal" v-if="lgAndUp"> Contact</v-btn>
 
     </v-app-bar>
     <v-main>
@@ -76,3 +77,10 @@ import Bild1 from '@/assets/Bild1.jpg'
 const drawer = ref(false)
 let {mdAndDown, lgAndUp, md, smAndDown} = useDisplay();
 </script>
+<style>
+.v-list-item--active {
+  color: black;
+!important;
+}
+
+</style>

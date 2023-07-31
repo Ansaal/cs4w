08f9b0ff5eb8dfaab03c57819ba667ddf434f513
Johannes Hulsch <johannes.hulsch@uvaro.com>
2023-07-30 22:08:52 -0700

    max height img

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="position: relative">
   <v-img
-    :src="Bild1" max-height="25vh" cover/>
+    :src="Bild1" max-height="400px" cover/>
   <div style="position: absolute; top: 10px; left: 10px; color: white">“the free soul is rare, but you know it when you see it - basically <br> because you feel good, very good, when you are near or with them.”
     <br >― Charles Bukowski</div>
   </div>
